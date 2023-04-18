5821925b8b6b695e5db064a13b14d67e08233d17
Yuhang Peng <peter.yuhangpeng@gmail.com>
2023-04-17 20:11:42 -0700

    Give the tips for break loop

--- ch1/char_count.c
+++ ch1/char_count.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+
+// Tips: press ctrl-d to give EOF and break the loop
+
 /* 
  * count characters in input; 1st version
  */
@@ -21,6 +24,9 @@ int main() {
   // cope with even bigger numbers by using double
   double nc;
 
+  // all of the work is done in the test and increment parts
+  // But the grammatical rules of C require that a for statement have a body
+  // The isolated semicolon, called a null statement, is there to satisfy that requirement
   for (nc = 0; getchar() != EOF; ++nc)
     ;
   printf("%.0f\n", nc);
