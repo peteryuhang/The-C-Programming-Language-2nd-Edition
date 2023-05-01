9607e37c9d7c95db117ecca37bae407a77c50354
Yuhang Peng <peter.yuhangpeng@gmail.com>
2023-05-01 07:02:07 -0700

    Add the separate files version of calculator program

---
+++ ch4/reverse_polish_notation/getch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* 
+ * getch delivers the next input character to be considered;
+ * ungetch remembers the characters put back on the input, so
+ * that subsequent calls to getch will return them before reading
+ * new input
+ */
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];      /* buffer for ungetch */
+int bufp = 0;           /* next free position in buf */
+
+int getch(void) {       /* get a (possibly pushed back) character */
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {     /* push character back on input */
+  if (bufp >= BUFSIZE)
+    printf("ungetch: too many characters");
+  else
+    buf[bufp++] = c;
+}
